3930c6c2b7426942f4e8fec3f4a694e87ea04622
Šarūnas <sgriskus2002@gmail.com>
2022-09-17 13:23:00 +0300

    split types into different files adjusted created types for DTO operations

---
+++ src/types/backEnd/LocationDTO.ts
@@ -0,0 +1,13 @@
+/**
+ * Data Transfer Object for @type {Location}.
+ */
+export type LocationDTO = {
+  /**
+   * Property equivalent to @see {Location.longitude}.
+   */
+  longitude?: number;
+  /**
+   * Property equivalent to @see {Location.latitude}.
+   */
+  latitude?: number;
+};
---
+++ src/types/backEnd/PollutedLocationDTO.ts
@@ -0,0 +1,35 @@
+import { LocationDTO } from "./LocationDTO";
+
+/**
+ * Data Transfer Object for @type {PollutedLocation}.
+ */
+export type PollutedLocationDTO = {
+  /**
+   * Property equivalent to @see {PollutedLocation.id}
+   */
+  id?: string;
+  /**
+   * Property equivalent to @see {PollutedLocation.location}
+   */
+  location?: LocationDTO;
+  /**
+   * Property equivalent to @see {PollutedLocation.radius}
+   */
+  radius?: number;
+  /**
+   * Property equivalent to @see {PollutedLocation.severity}
+   */
+  severity?: string;
+  /**
+   * Property equivalent to @see {PollutedLocation.spotted}
+   */
+  spotted?: string;
+  /**
+   * Property equivalent to @see {PollutedLocation.progress}
+   */
+  progress?: number;
+  /**
+   * Property equivalent to @see {PollutedLocation.notes}
+   */
+  notes?: string;
+};
