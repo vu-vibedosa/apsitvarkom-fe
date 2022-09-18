b1e16dae19afd6967bbabf967d015569df5e9f27
Vilius <vilius.bakutis23@gmail.com>
2022-09-18 20:02:16 +0300

    Minor fixes

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,6 @@
 import React from "react";
 import Example from "./components/Example";
-import Layout from "./components/layout/layout";
+import Layout from "./components/layout/Layout";
 
 const App: React.FC = () => {
   return (
--- src/components/layout/layout.tsx
+++ src/components/layout/layout.tsx
@@ -4,12 +4,8 @@ type Props = {
   children: React.ReactNode;
 };
 
-const Layout: React.FC<Props> = ({ children }: Props) => {
-  return (
-    <>
-      <div className="container mx-auto">{children}</div>;
-    </>
-  );
+const Layout: React.FC<Props> = ({ children }) => {
+  return <div className="container mx-auto">{children}</div>;
 };
 
 export default Layout;
