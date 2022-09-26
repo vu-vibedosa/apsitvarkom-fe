612651fbd87a5e2942a6d3cced4403d6e8418894
Vilius <vilius.bakutis23@gmail.com>
2022-09-26 23:01:20 +0300

    Create getAllMethod

--- src/backEndClient.ts
+++ src/backEndClient.ts
@@ -1,8 +1,11 @@
 import axios from "axios";
 import { ExampleModel } from "./components/Example";
+import PollutedLocationDTO from "./types/backEnd/PollutedLocationDTO";
 
-const getExample = () => {
+export const getExample = () => {
   return axios.get<ExampleModel>("/api/Example");
 };
 
-export default getExample;
+export const getAllPollutedLocations = () => {
+  return axios.get<PollutedLocationDTO>("/api/PollutedLocation/All");
+};
