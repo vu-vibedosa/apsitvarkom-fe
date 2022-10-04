1bd9cf487e3b3f1eb767587ceeb4f11ed240a583
BenasB <29711974+BenasB@users.noreply.github.com>
2022-10-04 15:19:53 +0300

    add dynamic severity icons and more fake locations

--- src/App.tsx
+++ src/App.tsx
@@ -5,23 +5,55 @@ import PollutedLocationCard from "./components/pollutedLocationCard/PollutedLoca
 import type { PollutedLocationType } from "./components/pollutedLocationCard/PollutedLocationCard";
 
 const App: React.FC = () => {
-  const pollutedLocation: PollutedLocationType = {
-    id: "a950ab10-d394-468b-b2ef-776135a65b96",
-    title: "Unknown Title",
-    location: {
-      longitude: 121.151,
-      latitude: 121.151,
+  const fakePollutedLocations: PollutedLocationType[] = [
+    {
+      id: "a950ab10-d394-468b-b2ef-776135a65b96",
+      title: "Title #1",
+      location: {
+        longitude: 121.151,
+        latitude: 121.151,
+      },
+      radius: 20,
+      severity: "low",
+      spotted: new Date("2022-09-22T09:00:29.4542617Z"),
+      progress: 66,
+      notes: "something should be written there :)",
     },
-    radius: 20,
-    severity: "low",
-    spotted: new Date("2022-09-22T09:00:29.4542617Z"),
-    progress: 66,
-    notes: "something should be written there :)",
-  };
+    {
+      id: "b950ab10-d394-468b-b2ef-776135a65b98",
+      location: {
+        longitude: 102.158,
+        latitude: 103.123,
+      },
+      radius: 7,
+      severity: "high",
+      spotted: new Date("2022-07-15T09:00:29.4542617Z"),
+      progress: 0,
+      notes: "Lorem ipsum",
+    },
+    {
+      id: "c950ab10-d394-468b-b2ef-776135a65b97",
+      title: "Title #2",
+      location: {
+        longitude: 112.327,
+        latitude: 97.842,
+      },
+      radius: 38,
+      severity: "moderate",
+      spotted: new Date("2022-08-22T09:00:29.4542617Z"),
+      progress: 100,
+      notes: "Hello world",
+    },
+  ];
+
   return (
     <Layout>
       <Example />
-      <PollutedLocationCard {...pollutedLocation} />
+      <div className="space-y-4 max-w-xl">
+        {fakePollutedLocations.map((pl) => (
+          <PollutedLocationCard {...pl} key={pl.id} />
+        ))}
+      </div>
     </Layout>
   );
 };
--- src/components/pollutedLocationCard/PollutedLocationCard.tsx
+++ src/components/pollutedLocationCard/PollutedLocationCard.tsx
@@ -2,7 +2,7 @@ import React from "react";
 import { MdDelete } from "react-icons/md";
 import { MdDeleteOutline } from "react-icons/md";
 import { MdOutlineRadar } from "react-icons/md";
-import PollutedLocation from "../../types/PollutedLocation";
+import PollutedLocation, { severityLevels } from "../../types/PollutedLocation";
 
 export type PollutedLocationType = PollutedLocation & {
   title?: string;
@@ -12,13 +12,19 @@ const PollutedLocationCard: React.FC<PollutedLocationType> = ({
   title,
   radius,
   progress,
+  severity,
 }) => {
   const defaultIconSize = 35;
+
+  const filledTrashIcons: number =
+    (severityLevels.findIndex((x) => x === severity) || 0) + 1;
+
+  const emptyTrashIcons: number = severityLevels.length - filledTrashIcons;
 
   return (
     <div className=" bg-white rounded-xl shadow-lg p-4 space-y-5 border border-gray-200">
       <div className="flex justify-between items-center">
-        <h2>{title || "Title is missing"}</h2>
+        <h2 className="text-lg">{title || "Title is missing"}</h2>
         <div className="flex space-x-2 items-center">
           <p className="text-sm text-blue-700">{progress || 0}%</p>
           <div className=" bg-gray-200 rounded-full h-2.5 w-24">
@@ -31,9 +37,12 @@ const PollutedLocationCard: React.FC<PollutedLocationType> = ({
       </div>
       <div className="flex justify-between items-center">
         <div className="flex -space-x-2">
-          <MdDelete size={defaultIconSize} />
-          <MdDeleteOutline size={defaultIconSize} />
-          <MdDeleteOutline size={defaultIconSize} />
+          {[...Array(filledTrashIcons)].map((index) => (
+            <MdDelete size={defaultIconSize} key={index} />
+          ))}
+          {[...Array(emptyTrashIcons)].map((index) => (
+            <MdDeleteOutline size={defaultIconSize} key={index} />
+          ))}
         </div>
         <div className="flex space-x-2 items-center">
           <p className="text-sm">{(radius || 0) + " m."}</p>
