405e4193bfb3d545835a73b87d0c770b2df6b4b3
XxDoMaSxX <57041752+XxDoMaSxX@users.noreply.github.com>
2022-10-04 13:43:22 +0300

    main card component with data displayed in (#14)
    * main card component with data displayed in
    
    * package-lock file to pass npm ci test
    
    * Progress bar percentage value
    
    * Using object instance insted recreating new one
    
    * minor changes for the component

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,27 @@
 import React from "react";
 import Example from "./components/Example";
 import Layout from "./components/layout/Layout";
+import PollutedLocationCard from "./components/pollutedLocationCard/PollutedLocationCard";
+import type { PollutedLocationType } from "./components/pollutedLocationCard/PollutedLocationCard";
 
 const App: React.FC = () => {
+  const pollutedLocation: PollutedLocationType = {
+    id: "a950ab10-d394-468b-b2ef-776135a65b96",
+    title: "Unknown Title",
+    location: {
+      longitude: 121.151,
+      latitude: 121.151,
+    },
+    radius: 20,
+    severity: "low",
+    spotted: new Date("2022-09-22T09:00:29.4542617Z"),
+    progress: 66,
+    notes: "something should be written there :)",
+  };
   return (
     <Layout>
       <Example />
+      <PollutedLocationCard {...pollutedLocation} />
     </Layout>
   );
 };
---
+++ src/components/pollutedLocationCard/PollutedLocationCard.tsx
@@ -0,0 +1,55 @@
+import React from "react";
+import { BsTrashFill } from "react-icons/bs";
+import { BsTrash } from "react-icons/bs";
+import { RiLogoutCircleRFill } from "react-icons/ri";
+import PollutedLocation from "../../types/PollutedLocation";
+
+export type PollutedLocationType = PollutedLocation & {
+  title?: string;
+};
+
+const PollutedLocationCard: React.FC<PollutedLocationType> = ({
+  title,
+  radius,
+  progress,
+}) => {
+  return (
+    <div className="mx-auto bg-white rounded-xl shadow-lg flex items-center space-x-4 sm:w-1/4 md:w-4/5 lg:w-1/4 px-2 pb-2 pt-2">
+      <div className="flex flex-col w-full">
+        <div className="flex flex-row space-x-4">
+          <div className="w-1/2 font-bold">
+            <h2>{title || "Title is missing"}</h2>
+          </div>
+          <div className="w-1/2">
+            <div className="flex justify-between mb-1">
+              <span className="text-sm font-medium text-blue-700 dark:text-white">
+                {progress || 0}%
+              </span>
+              <div className="w-full bg-gray-200 rounded-full h-2.5 dark:bg-gray-700 mt-1.5">
+                <div
+                  className="bg-blue-600 h-2.5 rounded-full"
+                  style={{ width: progress || 0 + "%" }}
+                ></div>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div className="flex flex-row justify-between mt-3">
+          <div className="flex flex-row justify-between">
+            <BsTrashFill size={35} />
+            <BsTrash size={35} />
+            <BsTrash size={35} />
+          </div>
+          <div className="w-1/5 content-end">
+            <div className="flex items-stretch">
+              <div className="mt-1">{radius + "m." || 0 + "m."}</div>
+              <RiLogoutCircleRFill size={35} />
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default PollutedLocationCard;
