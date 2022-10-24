0609b8926449ce89588d56e998b933e3a0a2a8c7
Šarūnas <sgriskus2002@gmail.com>
2022-10-24 22:04:40 +0300

    deleted the example from front end

--- src/backEndClient.ts
+++ src/backEndClient.ts
@@ -1,12 +1,5 @@
 import axios from "axios";
-import { ExampleModel } from "./components/Example";
 import PollutedLocationDTO from "./types/backEnd/PollutedLocationDTO";
-
-export const getExample = () => {
-  return axios.get<ExampleModel>(
-    process.env.REACT_APP_BACK_END_URL + "/api/Example"
-  );
-};
 
 export const getAllPollutedLocations = () => {
   return axios.get<PollutedLocationDTO[]>(
