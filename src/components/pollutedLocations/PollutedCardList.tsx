df0c56432190afef3fe266fe00ac56c6bc9146b4
XxDoMaSxX <dragunasdomas@gmail.com>
2022-11-15 01:31:24 +0200

    component responsiveness

--- src/components/pollutedLocations/PollutedCardList.tsx
+++ src/components/pollutedLocations/PollutedCardList.tsx
@@ -4,7 +4,7 @@ import PollutedLocationCard from "./PollutedLocationCard";
 const PollutedCardList = (list: PollutedLocation[]) => {
   return (
     <div className="space-y-4 max-w-xl overflow-y-auto">
-      <div className="sm:max-md:w-96 lg:max-2xl:w-full">
+      <div className="w-96 md:w-full">
         {list.map((location) => (
           <>
             <PollutedLocationCard {...location} key={location.id} />
