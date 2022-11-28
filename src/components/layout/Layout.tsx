c0d79be7824205b8249aff2202e3ea426fdc583d
BenasB <29711974+BenasB@users.noreply.github.com>
2022-11-28 16:18:57 +0200

    Layout h-screen mobile fix

--- src/components/layout/Layout.tsx
+++ src/components/layout/Layout.tsx
@@ -5,7 +5,11 @@ type Props = {
 };
 
 const Layout: React.FC<Props> = ({ children }) => {
-  return <div className="flex flex-col md:flex-row h-screen">{children}</div>;
+  return (
+    <div className="absolute inset-0">
+      <div className="flex flex-col md:flex-row h-screen">{children}</div>
+    </div>
+  );
 };
 
 export default Layout;
