ebccee2cd59f7cc5cec395c7d044c35404a3bdd5
BenasB <29711974+BenasB@users.noreply.github.com>
2022-12-06 15:23:15 +0200

    Header update

--- src/components/pollutedLocationPage/PollutedLocationDetails.tsx
+++ src/components/pollutedLocationPage/PollutedLocationDetails.tsx
@@ -1,8 +1,26 @@
 import React from "react";
-import PollutedLocation from "../../types/PollutedLocation";
+import { useTranslation } from "react-i18next";
+import PollutedLocation, {
+  severityLevels,
+  severityLevelsLocalized,
+} from "../../types/PollutedLocation";
 
-const PollutedLocationDetails: React.FC<PollutedLocation> = () => {
-  return <div>PollutedLocationDetails</div>;
+const PollutedLocationDetails: React.FC<PollutedLocation> = ({
+  radius,
+  severity,
+  progress,
+  notes,
+}) => {
+  const { t } = useTranslation();
+
+  return (
+    <div>
+      <div>{radius}</div>
+      <div>{severityLevelsLocalized(t)[severity || severityLevels[0]]}</div>
+      <div>{progress}</div>
+      <div>{notes}</div>
+    </div>
+  );
 };
 
 export default PollutedLocationDetails;
--- src/components/pollutedLocationPage/PollutedLocationHeader.tsx
+++ src/components/pollutedLocationPage/PollutedLocationHeader.tsx
@@ -3,35 +3,51 @@ import React from "react";
 import { useTranslation } from "react-i18next";
 import supportedLanguages from "../../languages";
 import PollutedLocation from "../../types/PollutedLocation";
-import { MdOutlineEdit } from "react-icons/md";
-import { MdOutlineDirections } from "react-icons/md";
+import {
+  MdOutlineDeleteForever,
+  MdOutlineEdit,
+  MdOutlineDirections,
+} from "react-icons/md";
 
-const PollutedLocationHeader: React.FC<PollutedLocation> = ({ location }) => {
+const PollutedLocationHeader: React.FC<PollutedLocation> = ({
+  location,
+  spotted,
+}) => {
   const { t } = useTranslation();
   const currentLanguage =
     supportedLanguages.find((x) => x === i18next.language) ||
     supportedLanguages[0];
 
   return (
-    <div className="my-2 md:my-8 flex flex-col md:flex-row space-y-2 md:space-x-8 md:items-center justify-between">
-      <div>
+    <div className="my-2 md:my-8 flex flex-col md:flex-row space-y-2 md:space-y-0 md:space-x-8 md:items-center justify-between">
+      <div className="flex flex-col">
         <h1 className="text-lg md:text-2xl font-semibold md:font-bold leading-5">
           {location?.title?.[currentLanguage] ||
             t("titleMissing", "Title is missing")}
         </h1>
+        <div className="text-sm text-gray-400">
+          {spotted?.toLocaleString(i18next.language)}
+        </div>
       </div>
-      <div className="flex space-x-4">
+      <div className="flex space-x-4 md:my-auto">
         <button
           type="button"
-          className="px-4 py-2 flex items-center rounded-md border border-gray-300 bg-white text-gray-700 shadow-sm md:hover:bg-gray-50"
+          className="px-4 py-2 flex text-sm md:text-base items-center rounded-md border border-gray-300 bg-white text-gray-700 shadow-sm md:hover:bg-gray-50"
         >
           <MdOutlineEdit className="text-xl mr-1" />
           {t("edit", "Edit")}
         </button>
+        <button
+          type="button"
+          className="px-4 py-2 flex text-sm md:text-base items-center rounded-md border border-red-300 bg-white text-red-700 shadow-sm md:hover:bg-red-50"
+        >
+          <MdOutlineDeleteForever className="text-xl mr-1" />
+          {t("delete", "Delete")}
+        </button>
         <a
           href={`https://www.google.com/maps/dir/?api=1&destination=${location?.coordinates?.latitude},${location?.coordinates?.longitude}`}
           type="button"
-          className="px-4 py-2 flex items-center rounded-md border border-gray-300 bg-white text-gray-700 shadow-sm md:hover:bg-gray-50"
+          className="px-4 py-2 flex text-sm md:text-base items-center rounded-md border border-gray-300 bg-white text-gray-700 shadow-sm md:hover:bg-gray-50"
         >
           <MdOutlineDirections className="text-xl mr-1" />
           {t("directions", "Directions")}
