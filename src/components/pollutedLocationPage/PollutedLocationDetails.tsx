b6b0607506f07ead9502bf21cc596450e7cc30d3
BenasB <29711974+BenasB@users.noreply.github.com>
2022-12-06 15:31:14 +0200

    Details table

--- src/components/pollutedLocationPage/PollutedLocationDetails.tsx
+++ src/components/pollutedLocationPage/PollutedLocationDetails.tsx
@@ -14,11 +14,45 @@ const PollutedLocationDetails: React.FC<PollutedLocation> = ({
   const { t } = useTranslation();
 
   return (
-    <div>
-      <div>{radius}</div>
-      <div>{severityLevelsLocalized(t)[severity || severityLevels[0]]}</div>
-      <div>{progress}</div>
-      <div>{notes}</div>
+    <div className="shadow md:rounded-lg">
+      <div className="border-t border-gray-200">
+        <dl>
+          <div className="bg-gray-50 px-4 py-5 md:grid md:grid-cols-3 md:gap-4 md:px-6">
+            <dt className="text-md font-medium text-gray-500">
+              {t("radius", "Radius") + " (" + t("meters", "meters") + ")"}
+            </dt>
+            <dd className="mt-1 text-md text-gray-900 md:col-span-2 md:mt-0">
+              {radius}
+            </dd>
+          </div>
+          <div className="bg-white px-4 py-5 md:grid md:grid-cols-3 md:gap-4 md:px-6">
+            <dt className="text-md font-medium text-gray-500">
+              {t("severity", "Severity")}
+            </dt>
+            <dd className="mt-1 text-md text-gray-900 md:col-span-2 md:mt-0">
+              {severityLevelsLocalized(t)[severity || severityLevels[0]]}
+            </dd>
+          </div>
+          <div className="bg-gray-50 px-4 py-5 md:grid md:grid-cols-3 md:gap-4 md:px-6">
+            <dt className="text-md font-medium text-gray-500">
+              {t("progress", "Progress")}
+            </dt>
+            <dd className="mt-1 text-md text-gray-900 md:col-span-2 md:mt-0">
+              {progress}
+            </dd>
+          </div>
+          {notes && (
+            <div className="bg-gray-50 px-4 py-5 md:grid md:grid-cols-3 md:gap-4 md:px-6">
+              <dt className="text-md font-medium text-gray-500">
+                {t("notes", "Notes")}
+              </dt>
+              <dd className="mt-1 text-md text-gray-900 md:col-span-2 md:mt-0">
+                {notes}
+              </dd>
+            </div>
+          )}
+        </dl>
+      </div>
     </div>
   );
 };
